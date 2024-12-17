b186bc3edb0e15c34ccdb8137617692e647b6cc9
Abhishek <hpatilabhi10@gmail.com>
2024-12-17 16:52:46 +0530

    Textfield for quantity

--- tradeapp/lib/src/features/ladder_view/presentation/pages/ladder_page.dart
+++ tradeapp/lib/src/features/ladder_view/presentation/pages/ladder_page.dart
@@ -4,7 +4,8 @@ import 'package:tradeapp/src/features/ladder_view/presentation/widgets/price_lad
 import 'package:tradeapp/src/widgets/custom_appbar.dart';
 
 class LadderPage extends StatelessWidget {
-  final TextEditingController quantityController = TextEditingController();
+  final TextEditingController quantityController =
+      TextEditingController(text: '1');
 
   LadderPage({super.key});
 
@@ -12,19 +13,22 @@ class LadderPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: CustomAppBar(
-          stockName: "HDFCBANK",
-          dayPnl: "+₹2.45",
-          pendingOrders: 1,
-          onBack: () => Navigator.pop(context),
-        ),
-        // body: PriceLadder(),
-        bottomNavigationBar: BottomBar(
-          onBuy: () {},
-          onSell: () {},
-          quantityController: quantityController,
-        ),
-      ),
+          resizeToAvoidBottomInset: true,
+          appBar: CustomAppBar(
+            stockName: "HDFCBANK",
+            dayPnl: "+₹2.45",
+            pendingOrders: 1,
+            onBack: () => Navigator.pop(context),
+          ),
+          body: PriceLadder(),
+          bottomNavigationBar: Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: BottomBar(
+                onBuy: () {},
+                onSell: () {},
+                quantityController: quantityController,
+              ))),
     );
   }
 }
--- tradeapp/lib/src/features/ladder_view/presentation/widgets/bottom_bar.dart
+++ tradeapp/lib/src/features/ladder_view/presentation/widgets/bottom_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tradeapp/src/constants/app_colors.dart';
+import 'package:tradeapp/src/constants/app_sizes.dart';
+import 'package:tradeapp/src/theme/app_theme.dart';
 import 'package:tradeapp/src/widgets/custom_button.dart';
 
 class BottomBar extends StatelessWidget {
@@ -29,14 +31,44 @@ class BottomBar extends StatelessWidget {
               onTap: onBuy,
               text: 'Buy MKT',
               buttonColor: AppColor.greenColor),
-          SizedBox(
+          Container(
             height: 36,
             width: 102,
-            child: TextField(
-              controller: quantityController,
-              keyboardType: TextInputType.number,
-              textAlign: TextAlign.center,
-              decoration: const InputDecoration(border: OutlineInputBorder()),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(AppSizes.appBorderRadius),
+                border: Border.all(
+                  color: AppColor.whiteColor,
+                )),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 5),
+                  child: SizedBox(
+                    height: 20,
+                    width: 30,
+                    child: TextField(
+                      controller: quantityController,
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
+                      style: myTextTheme.bodySmall!
+                          .copyWith(fontSize: 12, color: AppColor.whiteColor),
+                      decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: AppSizes.appPadding),
+                          border: UnderlineInputBorder()),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'Qty',
+                  style: myTextTheme.bodySmall!
+                      .copyWith(color: AppColor.whiteColor, fontSize: 12),
+                )
+              ],
             ),
           ),
           CustomButton(
